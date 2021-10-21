4c46e94039b6c3629e9cb772552cd9f37a0c2e3a
David <davidm2@wit.edu>
2021-10-21 08:29:22 -0400

    lab3

--- lab2.py
+++ lab2.py
@@ -1,5 +1,19 @@
 import pygame
+from Ball import *
 from pygame import draw
+from random import randint
+import random
+import numpy as np
+from pygame import color
+
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+WIDTH = 800
+HEIGHT = 480
+BORDER = 20
+COLOR = pygame.Color(255,0,245)
+VELOCITY = 5
 
 class Wall():
     def __init__(self, x, y, width, height, color, screen):
@@ -22,27 +36,39 @@ class Wall():
 def main(): 
     pygame.init()
 
-    pygame.display.set_caption("lab2")
-    WIDTH = 800
-    HEIGHT = 480
-    BORDER = 20
-    COLOR = pygame.Color(255,0,245)
+    pygame.display.set_caption("pong")
+
+
+
+
+    surface = pygame.display.set_mode((WIDTH,HEIGHT))
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
     wall_top = Wall(0,0,WIDTH,BORDER,COLOR,screen)
     wall_bottom = Wall(0,HEIGHT-BORDER,WIDTH,BORDER,COLOR,screen)
     wall_left = Wall(0,0,BORDER,HEIGHT,COLOR,screen)
 
-    wall_top.draw()
-    wall_bottom.draw()
-    wall_left.draw()
+    
+    #ball init
+    x0 = WIDTH - Ball.RADIUS
+    y0 = HEIGHT//2 
+    angle = random.randrange(-45,45)
+    vx0 = -VELOCITY * np.abs(np.cos(angle))
+    vy0 = VELOCITY * np.abs(np.sin(angle))
+    print(angle)
+    ball = Ball(x0,y0,vx0,vy0,surface,COLOR,BLACK)
 
+    ball.show(COLOR)
     pygame.display.update()
+    
+
 
 
     # define a variable to control the main loop
     running = True
-        
+
+    clock = pygame.time.Clock()
+    
     # main loop
     while running:
         # event handling, gets all event from the event queue
@@ -51,6 +77,16 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            #all_sprites_list.update()
+        screen.fill(BLACK)
+        wall_left.draw()
+        wall_top.draw()
+        wall_bottom.draw()
+        ball.update()
+
+
+        pygame.display.flip()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
